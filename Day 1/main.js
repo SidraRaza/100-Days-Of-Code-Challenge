c960f7e85dcafd19d9d0eab0267e60ad44d16b58
Ahed880011@gmail.com <Ahed880011@gmail.com>
2024-06-27 20:45:12 +0500

    first commit

---
+++ Day 1/main.js
@@ -0,0 +1,10 @@
+// Question 1: Personal Message: Store a person’s name in a variable and print a message to them,
+//  like “Hello Asharib, would you like to learn some TypeScript today?”
+var nam = "Sidra Raza";
+console.log("Hello ".concat(nam, ", would you like to learn some TypeScript today?"));
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+// uppercase, and titlecase.
+var Cases = "Sidra";
+console.log(Cases.toLowerCase());
+console.log(Cases.toUpperCase());
+console.log(Cases.charAt(0).toUpperCase() + Cases.slice(1).toLowerCase());
---
+++ Day 1/main.ts
@@ -0,0 +1,15 @@
+// Question 1: Personal Message: Store a person’s name in a variable and print a message to them,
+//  like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let nam="Sidra Raza";
+console.log(`Hello ${nam}, would you like to learn some TypeScript today?`);
+
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, 
+// uppercase, and titlecase.
+
+let Cases: string = "Sidra"; 
+
+console.log(Cases.toLowerCase());
+console.log(Cases.toUpperCase()); 
+console.log(Cases.charAt(0).toUpperCase() + Cases.slice(1).toLowerCase()); 
+
